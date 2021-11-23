10b8b5856ebb62b8aea13dfc63a4fd47febcb596
nluuarch@gmail.com <nluuarch@gmail.com>
2021-11-23 14:25:17 -0800

    commit tree c44013773f6491e7f41384bc5f4ac1c79d4e1d2c

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,7 @@
 import React from "react";
-
-function NavBar() {
-  return (
-    <nav>
-      <a href="#home">I'm a link!</a>
-    </nav>
-  );
-}
-
-function Home() {
-  return (
-    <div id="home">
-      <h1>Home</h1>
-    </div>
-  );
-}
+import NavBar from "./NavBar";
+import Home from "./Home";
+import About from "./About";
 
 {/* write an <About> component here */}
 
@@ -23,8 +10,8 @@ function App() {
   return (
     <div>
       <NavBar />
-      {/* add the <Home> component here */}
-      {/* add your <About> component here */}
+      <Home />
+      <About />
     </div>
   );
 }
